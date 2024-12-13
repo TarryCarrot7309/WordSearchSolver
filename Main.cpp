aea6dca0926a950760a00086d03cb6329b8400d1
TarryCarrot <brian_speciaity@outlook.com>
2024-12-13 15:48:10 +0100

    Added Grid and initial variables

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 std::string word_find;
+std::vector <std::string> words = {"BRAIN","BONES","TISSUE", "MUSCLE", "BLOOD", "NERVES", "CELL"};
+std::vector<std::vector<char>> grid = {
+        {'B', 'K', 'D', 'S', 'U', 'B', 'O', 'N', 'E', 'S', 'C', 'C'},
+        {'H', 'R', 'J', 'S', 'N', 'E', 'R', 'V', 'E', 'S', 'C', 'C'},
+        {'Y', 'T', 'A', 'L', 'U', 'O', 'Q', 'A', 'A', 'B', 'F', 'T'},
+        {'W', 'L', 'Q', 'I', 'Q', 'O', 'C', 'E', 'L', 'L', 'Y', 'I'},
+        {'F', 'E', 'I', 'J', 'N', 'W', 'E', 'Z', 'Y', 'H', 'X', 'S'},
+        {'U', 'B', 'L', 'O', 'O', 'D', 'X', 'V', 'P', 'U', 'I', 'S'},
+        {'T', 'J', 'H', 'Z', 'B', 'R', 'O', 'P', 'J', 'B', 'L', 'U'},
+        {'A', 'M', 'U', 'S', 'C', 'L', 'E', 'Q', 'D', 'F', 'K', 'E'}
+    };
+
 
 int main()
 {

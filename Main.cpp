8b49625ac36e17b20b6218194c1bb5255be3ef4f
TarryCarrot <brian_speciaity@outlook.com>
2024-12-16 15:12:20 +0100

    added basic funstions

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,12 @@
 #include <string>
 #include <vector>
 
+struct Coordinate {
+    int x; // Row
+    int y; // Column
+
+    Coordinate(int x_value, int y_value) : x(x_value), y(y_value) {}
+};
 std::string word_find;
 std::vector <std::string> words = {"BRAIN","BONES","TISSUE", "MUSCLE", "BLOOD", "NERVES", "CELL"};
 std::vector<std::vector<char>> grid = {
@@ -14,7 +20,37 @@ std::vector<std::vector<char>> grid = {
         {'T', 'J', 'H', 'Z', 'B', 'R', 'O', 'P', 'J', 'B', 'L', 'U'},
         {'A', 'M', 'U', 'S', 'C', 'L', 'E', 'Q', 'D', 'F', 'K', 'E'}
     };
+std::vector<Coordinate> findAll(const std::vector<std::vector<char>> &grid, char word) {
+    std::vector<Coordinate> coordinates; // To store all coordinates
 
+    // Loop through the grid
+    for (int row = 0; row < grid.size(); row++) {
+        for (int col = 0; col < grid[row].size(); col++) {
+            if (grid[row][col] == word) {
+                // Add the coordinate to the vector
+                coordinates.emplace_back(row, col);
+            }
+        }
+    }
+
+    return coordinates;
+}
+Coordinate check_horizontal(std::vector <Coordinate> &coordinates, std::string word_find)
+{
+
+    for (int i = 0; i < coordinates.size(); i++)
+    {
+        low = coordinates[i].y + 1;
+        up = coordinates[i]. y - 1;
+        lower = grid
+        if (coordinates[i].x + 1 == )
+        {
+            /* code */
+        }
+        
+    }
+    
+}
 
 int main()
 {
